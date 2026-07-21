bf25d07e70fdf2be7f6d42e05c826024fc66ad71
Graham Stark <graham.stark@virtual-worlds.biz>
2026-07-21 21:52:53 +0100

    Misc cosmetic stuff; start of submit handling.

--- models/_includes/js/handle_submissions.js
+++ models/_includes/js/handle_submissions.js
@@ -1,4 +1,5 @@
 async function resetParams(){
+    const uid = getUID();
 
 }
 
@@ -23,10 +24,16 @@ async function submitParams(){
 
 async function submitRun(){
     await submitParams();
+    const uid = getUID();
     const url = [API,"run","submit",MODEL,EDITION,SUBSYS].join("/") + "?uid="+uid;
+    console.log( "submitting: " + url )
     try{
-        const response = await fetch(url, {method: "GET"});
-        console.log(await response.json());
+        await fetch(url)
+        .then( response => response.json())
+        .then( data => {
+            console.log("data = " + data);
+        });
+        // (;uid=user.user_id, runid=runrec.run_id, errors=errs, output_is_cached=runrec.output_is_cached )
     } catch(e) {
         console.error(e);
     }
