0846f1b9f48fc65de1f89205591fdfa79e18e656
Graham Stark <graham.stark@virtual-worlds.biz>
2026-08-20 15:22:49 +0100

    More BI Code.

--- models/_includes/js/bi-params-edit.js
+++ models/_includes/js/bi-params-edit.js
@@ -1,12 +1,13 @@
 function scrapeData(){
     var tbs = scrapeTax("tax");
-    // console.log( "tbs=" + tbs );
     var nbs = scrapeTax( "ni");
+    // console.log( "tbs=" + tbs );
     var data = {
         taxrates: tbs[0],
         taxbands: tbs[1],
         nirates: nbs[0],
         nibands: nbs[1],
+
         taxallowance: parseFloat($( "#taxallowance" ).val()),
         child_benefit: parseFloat( $( "#child_benefit" ).val()),
         pension: parseFloat( $( "#pension" ).val()),
@@ -24,11 +25,6 @@ function populateForm( pars, defaults ){
         pars.taxbands,
         defaults.taxrates,
         defaults.taxbands );
-    initialiseTable( "ni",
-        pars.nirates,
-        pars.nibands,
-        defaults.nirates,
-        defaults.nibands );
     setVal( 'taxallowance', pars.taxallowance, defaults.taxallowance);
     setVal( 'child_benefit', pars.child_benefit, defaults.child_benefit );
     setVal( 'pension', pars.pension, defaults.pension );
@@ -37,3 +33,33 @@ function populateForm( pars, defaults ){
     setVal( 'uc_single', pars.uc_single, defaults.uc_single );
     setVal( 'uc_taper', pars.uc_taper, defaults.uc_taper );
 }
+
+
+/*
+@tags mutable struct UBIParams{Float64} <: Subsys
+    abolished :: Bool & (edit=(; label="Don't Have A UBI (Please!)"))
+    taxrates :: Vector{Float64} & (edit=(; label="Rates", min=0.0, max=100.0, group="Scottish Income Tax ", unit="%", prec=2))
+    taxbands :: Vector{Float64} & (edit=(; label="Thresholds", min=0.0, agroup="Scottish Income Tax", unit="£s pa", prec=0 ))
+    nirates :: Vector{Float64} & (edit=(; label="Rates", min=0.0, max=100.0, group="Employee National Insurance", unit="%", prec=2))
+    nibands :: Vector{Float64} & (edit=(; label="Bands", min=0.0, agroup="Employee National Insurance", unit="£s pw", prec=2))
+    taxallowance :: Float64  & (edit=(; label="Income Tax Allowance", min=0.0, unit="£s pa", prec=0))
+
+    abolish_uc :: Bool & (edit=(; label="Abolish Universal Credit"))
+    abolish_sickness_bens :: Bool & (edit=(; label="Abolish Sickness and Disablement Benefits?"))
+    abolish_jsa_esa:: Bool & (edit=(; label="Abolish Contributory ESA/JSA?"))
+    abolish_pensions :: Bool & (edit=(; label="Abolish The State Pension"))
+    abolish_housing :: Bool & (edit=(; label="Don't Meet Housing Costs of Low Income Families (Housing Benefit, Housing Component of Universal Credit)?"))
+    abolish_others :: Bool & (edit=(; label="Abolish All Other Benefits?"))
+
+    ubi_as_mt_income :: Bool & (edit=(; label="Treat The UBI As Income for Means-Tested Benefits?"))
+    ubi_taxable :: Bool & (edit=(; label="Make the UBI Taxable?"))
+
+    adult_amount :: Float64 & (edit=(; label="UBI: Amount Per Adult", min=0.0, unit="£s pw", prec=2))
+    child_amount :: Float64 & (edit=(; label="UBI: Amount Per Child", min=0.0, unit="£s pw", prec=2))
+    universal_pension :: Float64 & (edit=(; label="UBI: Amount Per Pension Age Person", min=0.0, unit="£s pw", prec=2))
+    adult_age :: Int & (edit=(; label="UBI: Age of Adulthood", min=0, max=21, unit="Years"))
+    retirement_age :: Int & (edit=(; label="UBI: Age of Retirement", min=50, unit="Years"))
+    # mt_bens_treatment :: UBEntitlement & (edit=(; label="UBI: How to treat Means-Tested Benefits", options=["1"])
+end
+
+*/
---
+++ models/bi-dev.md
@@ -0,0 +1,12 @@
+---
+title: Scotben - Basic Incomes
+author: graham.stark@virtual-worlds.scot
+layout: bi-sim
+permalink: /bi-dev/
+order: 1
+category: Scotland
+whichapi: http://microapi-dev/
+---
+
+BI SIM Stuff
+
